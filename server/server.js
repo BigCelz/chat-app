41c001f4fe1c13ffbb5613c478fd7dd87f7bedf6
BigCelz <stanleychilaka22270@gmail.com>
2026-03-07 19:05:46 +0100

    fix 401 issue

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const allowedOrigins = [
 const corsOptions = {
   origin: allowedOrigins,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  allowedHeaders: ["Content-Type", "Authorization", "token"],
   credentials: true,
 };
 
